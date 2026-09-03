bc2a646893468c9faa0f164daef8ca221dfa8e08
aretaneilaputri <retaaneila@gmail.com>
2026-09-03 09:15:57 +0700

    BukuStruktural.java

---
+++ BukuStruktural.java
@@ -0,0 +1,157 @@
+public class BukuStruktural {
+
+    // Function untuk meminjam buku
+    static void pinjamBuku(String judul) {
+        System.out.println(judul + " : Buku sedang dipinjam.");
+    }
+
+    // Function untuk mengembalikan buku
+    static void kembalikanBuku(String judul) {
+        System.out.println(judul + " : Buku telah dikembalikan.");
+    }
+
+    public static void main(String[] args) {
+
+        // Buku 1
+        String judul1 = "Laut Bercerita";
+        String penulis1 = "Leila S.Chudori";
+        int halaman1 = 394;
+
+        // Buku 2
+        String judul2 = "Sunset Bersama Rosie";
+        String penulis2 = "Tere Liye";
+        int halaman2 = 410;
+
+        // Buku 3
+        String judul3 = "Pukul Setengah Lima";
+        String penulis3 = "Rintik Sedu";
+        int halaman3 = 208;
+
+        // Buku 4
+        String judul4 = "Hujan";
+        String penulis4 = "Tere Liye";
+        int halaman4 = 320;
+
+        // Buku 5
+        String judul5 = "Cantik Itu Luka";
+        String penulis5 = "Eka Kurniawan";
+        int halaman5 = 508;
+
+        // Buku 6
+        String judul6 = "Perahu Kertas";
+        String penulis6 = "Dee Lestari";
+        int halaman6 = 444;
+
+        // Buku 7
+        String judul7 = "Mariposa";
+        String penulis7 = "Luluk HF";
+        int halaman7 = 496;
+
+        // Buku 8
+        String judul8 = "Sabtu Bersama Bapak";
+        String penulis8 = "Adhitya Mulya";
+        int halaman8 = 278;
+
+        // Buku 9
+        String judul9 = "Malioboro at Midnight";
+        String penulis9 = " Skysphire";
+        int halaman9 = 400;
+
+        // Buku 10
+        String judul10 = "Home Sweet Loan";
+        String penulis10 = "Almira Bastari";
+        int halaman10 = 312;
+
+
+        // Pemanggilan function Buku 1
+        System.out.println("Buku 1");
+        System.out.println("Judul   : " + judul1);
+        System.out.println("Penulis : " + penulis1);
+        System.out.println("Halaman : " + halaman1);
+        pinjamBuku(judul1);
+        kembalikanBuku(judul1);
+        System.out.println();
+
+        // Pemanggilan function Buku 2
+        System.out.println("Buku 2");
+        System.out.println("Judul   : " + judul2);
+        System.out.println("Penulis : " + penulis2);
+        System.out.println("Halaman : " + halaman2);
+        pinjamBuku(judul2);
+        kembalikanBuku(judul2);
+        System.out.println();
+
+        // Pemanggilan function Buku 3
+        System.out.println("Buku 3");
+        System.out.println("Judul   : " + judul3);
+        System.out.println("Penulis : " + penulis3);
+        System.out.println("Halaman : " + halaman3);
+        pinjamBuku(judul3);
+        kembalikanBuku(judul3);
+        System.out.println();
+
+        // Pemanggilan function Buku 4
+        System.out.println("Buku 4");
+        System.out.println("Judul   : " + judul4);
+        System.out.println("Penulis : " + penulis4);
+        System.out.println("Halaman : " + halaman4);
+        pinjamBuku(judul4);
+        kembalikanBuku(judul4);
+        System.out.println();
+
+        // Pemanggilan function Buku 5
+        System.out.println("Buku 5");
+        System.out.println("Judul   : " + judul5);
+        System.out.println("Penulis : " + penulis5);
+        System.out.println("Halaman : " + halaman5);
+        pinjamBuku(judul5);
+        kembalikanBuku(judul5);
+        System.out.println();
+
+        // Pemanggilan function Buku 6
+        System.out.println("Buku 6");
+        System.out.println("Judul   : " + judul6);
+        System.out.println("Penulis : " + penulis6);
+        System.out.println("Halaman : " + halaman6);
+        pinjamBuku(judul6);
+        kembalikanBuku(judul6);
+        System.out.println();
+
+        // Pemanggilan function Buku 7
+        System.out.println("Buku 7");
+        System.out.println("Judul   : " + judul7);
+        System.out.println("Penulis : " + penulis7);
+        System.out.println("Halaman : " + halaman7);
+        pinjamBuku(judul7);
+        kembalikanBuku(judul7);
+        System.out.println();
+
+        // Pemanggilan function Buku 8
+        System.out.println("Buku 8");
+        System.out.println("Judul   : " + judul8);
+        System.out.println("Penulis : " + penulis8);
+        System.out.println("Halaman : " + halaman8);
+        pinjamBuku(judul8);
+        kembalikanBuku(judul8);
+        System.out.println();
+
+        // Pemanggilan function Buku 9
+        System.out.println("Buku 9");
+        System.out.println("Judul   : " + judul9);
+        System.out.println("Penulis : " + penulis9);
+        System.out.println("Halaman : " + halaman9);
+        pinjamBuku(judul9);
+        kembalikanBuku(judul9);
+        System.out.println();
+
+        // Pemanggilan function Buku 10
+        System.out.println("Buku 10");
+        System.out.println("Judul   : " + judul10);
+        System.out.println("Penulis : " + penulis10);
+        System.out.println("Halaman : " + halaman10);
+        pinjamBuku(judul10);
+        kembalikanBuku(judul10);
+        System.out.println();
+    }
+}
+
